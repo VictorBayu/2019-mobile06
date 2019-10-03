bb512bcace8bfe1b500981c5ee6bc080ec48c6c0
VictorBayu <victorbayu85@gmail.com>
2019-10-03 23:09:48 +0700

    memperbaiki fungsi onCreateView pada BrocaIndexFragment.java

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -10,21 +10,26 @@ import android.view.MenuItem;
 import id.ac.polinema.idealbodyweight.Fragments.AboutFragment;
 import id.ac.polinema.idealbodyweight.Fragments.BrocaIndexFragment;
 import id.ac.polinema.idealbodyweight.Fragments.MenuFragment;
+import id.ac.polinema.idealbodyweight.Fragments.ResultFragment;
 
-public class MainActivity extends AppCompatActivity implements MenuFragment.OnFragmentInteractionListener, BrocaIndexFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements
+		MenuFragment.OnFragmentInteractionListener,
+		BrocaIndexFragment.OnFragmentInteractionListener,
+		ResultFragment.OnFragmentInteractionListener {
 
 	// Deklarasikan atribut Fragment di sini
 	private AboutFragment aboutFragment;
-	private  MenuFragment menuFragment;
 	private BrocaIndexFragment brocaIndexFragment;
+	private ResultFragment resultFragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		aboutFragment = AboutFragment.newInstance("Victor Bayu");
-		menuFragment = new MenuFragment();
+		MenuFragment menuFragment = new MenuFragment();
 		brocaIndexFragment = new BrocaIndexFragment();
+		resultFragment = new ResultFragment();
 		getSupportFragmentManager().beginTransaction()
 				.replace(R.id.fragment_container, menuFragment)
 				.commit();
@@ -61,8 +66,19 @@ public class MainActivity extends AppCompatActivity implements MenuFragment.OnFr
 
 	}
 
-	@Override
-	public void onFragmentInteraction(float index) {
 
+	@Override
+	public void onCalculateBrocaIndexClicked(float index) {
+		resultFragment.setInformation(String.format("Your ideal weight is %.2f kg", index));
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.fragment_container, resultFragment)
+				.commit();
+	}
+
+	@Override
+	public void onTryAgainButtonClicked(String tag) {
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.fragment_container, brocaIndexFragment)
+				.commit();
 	}
 }
